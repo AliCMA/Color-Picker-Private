407305e53cfa852536181817d3b1598e185ba0aa
AliCMA <32778@ma-web.nl>
2023-02-17 12:08:05 +0100

    klaar met video 4

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
-class ColorCard{
+class ColorCard {
     id;
     color;
     addToList;
+    htmlElement;
 
-    constructor(newId, newColor, addToList){
+    constructor(newId, newColor, addToList) {
+        /* Setting properties*/
         this.id = newId;
         this.color = newColor;
         this.addToList = addToList;
-    }
 
-    render(){
-        const listItemToBeRendered = document.createElement("li");
-        listItemToBeRendered.classList = "colors__color";
 
+        /* Make htmlElement to render */
+        this.htmlElement = document.createElement("li");
+        this.htmlElement.classList = "colors__color";
         const figureToBeRendered = document.createElement("figure");
         figureToBeRendered.classList = "colors__circle";
-
         figureToBeRendered.style.background = this.color;
-        listItemToBeRendered.appendChild(figureToBeRendered);
-        this.addToList.appendChild(listItemToBeRendered);
+        this.htmlElement.appendChild(figureToBeRendered);
+        this.element.onclick = onHTMLElementClicked();
+        this.render();
+
+    }
+
+    onHTMLElementClicked = function(){
+        console.log("Ik ben geklikt" + this.color);
+    }
+
+    render() {
+        this.addToList.appendChild(this.htmlElement);
     }
 }
 
 const test = new ColorCard(101, "rgba(255,0,0,1)", document.getElementById("js--colors"));
-test.render();
